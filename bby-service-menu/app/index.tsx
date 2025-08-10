e806d7c8d7d0ed990e8334b07deb387ba7442fe1
Mya Von Behren <mvonbehr@depaul.edu>
2025-08-10 14:40:40 -0400

    test service item

--- bby-service-menu/app/index.tsx
+++ bby-service-menu/app/index.tsx
@@ -1,16 +1,24 @@
 import { Text, View } from "react-native";
+import ServiceItem from "../components/ServiceMenu/ServiceItem";
+import { getAllServices } from "../data/mockServices";
+import Colors from "../constants/Colors";
 
 export default function Index() {
+  const mockServices = getAllServices();
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: Colors.background.primary,
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Text>Hello</Text>
+      <ServiceItem service={mockServices[0]} onPress={() => {}} onMorePress={() => {}} />
+
     </View> 
   );
 }
