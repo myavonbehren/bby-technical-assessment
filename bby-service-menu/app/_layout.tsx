2c55c8b640fdf14c821df1cddd3072f90f4c180e
Mya Von Behren <mvonbehr@depaul.edu>
2025-08-10 16:28:08 -0400

    Wrap RootLayout in GestureHandlerRootView to enable gesture handling and improve user interaction experience.

--- bby-service-menu/app/_layout.tsx
+++ bby-service-menu/app/_layout.tsx
@@ -1,18 +1,21 @@
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Colors from "../constants/Colors";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen 
-        name="index" 
-        options={{
-          title: "Services",
-          headerStyle: { backgroundColor: Colors.background.primary },
-          headerTintColor: Colors.text.primary,
-          headerShadowVisible: false,
-        }}
-      />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen 
+          name="index" 
+          options={{
+            title: "Services",
+            headerStyle: { backgroundColor: Colors.background.primary },
+            headerTintColor: Colors.text.primary,
+            headerShadowVisible: false,
+          }}
+        />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
