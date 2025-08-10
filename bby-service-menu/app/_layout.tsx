2d8112f089bb50309999fb9522580852c65061e7
Mya Von Behren <mvonbehr@depaul.edu>
2025-08-10 15:15:40 -0400

    Update RootLayout and Index components to enhance navigation and styling; add header options for Services screen and streamline layout in Index.

--- bby-service-menu/app/_layout.tsx
+++ bby-service-menu/app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import Colors from "../constants/Colors";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen 
+        name="index" 
+        options={{
+          title: "Services",
+          headerStyle: { backgroundColor: Colors.background.primary },
+          headerTintColor: Colors.text.primary,
+          headerShadowVisible: false,
+        }}
+      />
+    </Stack>
+  );
 }
--- bby-service-menu/app/index.tsx
+++ bby-service-menu/app/index.tsx
@@ -1,19 +1,16 @@
+// index.tsx
 import { View } from "react-native";
-import Colors from "../constants/Colors";
+import { GlobalStyles } from "../constants/Styles";
 import ServiceScreen from "../screens/ServiceScreen";
+import Colors from "@/constants/Colors";
 
 export default function Index() {
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: Colors.background.primary,
-      }}
-    >
-      <ServiceScreen />
-    </View> 
-  );
+    return (
+        <View style={{
+          flex: 1,
+          backgroundColor: Colors.background.primary,
+        }}>
+            <ServiceScreen />
+        </View> 
+    );
 }
