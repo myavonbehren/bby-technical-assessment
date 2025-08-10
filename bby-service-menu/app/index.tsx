b507f98e48ebfb96063134852cb5b00d35cd622a
Mya Von Behren <mvonbehr@depaul.edu>
2025-08-10 16:03:52 -0400

    add horizontal padding for improved layout

--- bby-service-menu/app/index.tsx
+++ bby-service-menu/app/index.tsx
@@ -1,7 +1,7 @@
 // index.tsx
 import { View } from "react-native";
 import { GlobalStyles } from "../constants/Styles";
-import ServiceScreen from "../screens/ServiceScreen";
+import ServiceScreen from "@/screens/ServiceScreen";
 import Colors from "@/constants/Colors";
 
 export default function Index() {
@@ -9,6 +9,7 @@ export default function Index() {
         <View style={{
           flex: 1,
           backgroundColor: Colors.background.primary,
+          paddingHorizontal: 10
         }}>
             <ServiceScreen />
         </View> 
